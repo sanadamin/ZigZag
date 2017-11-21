97a0978c92919ae7a83eb7ac6299ba2d6e626b4b
DESKTOP-MC1EC21\SanadAmin <sanad.amin@gmail.com>
2017-11-21 19:03:02 +0200

    Platform and ball control

---
+++ New Unity Project/Assets/CameaFollower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameaFollower : MonoBehaviour {
+    public GameObject ball;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
